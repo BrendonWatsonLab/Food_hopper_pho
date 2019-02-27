22ec68a1939978f6fd9ff35d70ef74969c20c73c
Pho Hale <halechr@umich.edu>
2019-02-27 12:20:16 -0500

    Remove unused "TTLPIN" variable

--- Food_hopper_pho.ino
+++ Food_hopper_pho.ino
@@ -14,12 +14,9 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
  * The getStepper(#steps, portNumber) command uses portNumber=2 for M3 or M4.
  */
 #define IS_DIAGNOSTIC_MODE true //IS_DIAGNOSTIC_MODE: if this value is true the system will operate continuously, ignoring the beam break sensor. This serves to allow testing. This value should be false outside of testing.
-
- 
 Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
 #define LEDPIN 13 // Pin 13: Arduino has an LED connected on pin 1
 #define SENSORPIN 4 // SENSORPIN: This pin is connected by a green wire to the beam-break sensor's "SIG" pin.
-#define TTLPIN 13
 
 // variables will change:
 int sensorState = 0, lastState=0;         // variable for reading the pushbutton status
@@ -42,9 +39,7 @@ int moveOperationCounter = 0; // This variable keeps track of the total number o
  
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(LEDPIN, OUTPUT);  
-  pinMode(TTLPIN, OUTPUT);
-  digitalWrite(TTLPIN, LOW);
+  pinMode(LEDPIN, OUTPUT);
   // initialize the sensor pin as an input:
   pinMode(SENSORPIN, INPUT);     
   digitalWrite(SENSORPIN, HIGH); // turn on the pullup
@@ -58,6 +53,7 @@ void setup() {
   // The setSpeed() function controls the power level delivered to the motor. 
   myMotor->setSpeed(StepperSpeed); //The speed parameter is a value between 0 and 255.
 }
+
 void loop(){
   // read the state of the IR break beam sensor:
   sensorState = digitalRead(SENSORPIN);
@@ -68,21 +64,18 @@ void loop(){
   if ((sensorState == LOW) || IS_DIAGNOSTIC_MODE) {
     //delay(40);
     digitalWrite(LEDPIN, HIGH); // Turn status LED on
-    digitalWrite(TTLPIN, HIGH);    // make motor turn
-    if(digitalRead(TTLPIN)==HIGH){
-      // The "%" operation is "modulus". The statement checks whether the moveOperationCounter is evenly divisible by ConsecutiveSameDirectionMovements. If it is, it performs the first case, otherwise the second.
-      if (((moveOperationCounter % ConsecutiveSameDirectionMovements) == 0)) {
-        //Every "ConsecutiveSameDirectionMovements + 1" steps we move counter-clockwise.
-        Serial.println("Moving: Counter-Clockwise " + String(moveOperationCounter));
-        myMotor->step(25, BACKWARD, NumberOfStepperCoilsActivated); // Changed from SINGLE to DOUBLE for extra torque
-      }
-      else {
-        // Otherwise we move in the traditional (clockwise) direction
-        Serial.println("Moving: Clockwise " + String(moveOperationCounter));
-        myMotor->step(25, FORWARD, NumberOfStepperCoilsActivated);
-      }
-      moveOperationCounter++; // Increment the counter
+    // The "%" operation is "modulus". The statement checks whether the moveOperationCounter is evenly divisible by ConsecutiveSameDirectionMovements. If it is, it performs the first case, otherwise the second.
+    if (((moveOperationCounter % ConsecutiveSameDirectionMovements) == 0)) {
+      //Every "ConsecutiveSameDirectionMovements + 1" steps we move counter-clockwise.
+      Serial.println("Moving: Counter-Clockwise " + String(moveOperationCounter));
+      myMotor->step(25, BACKWARD, NumberOfStepperCoilsActivated); // Changed from SINGLE to DOUBLE for extra torque
     }
+    else {
+      // Otherwise we move in the traditional (clockwise) direction
+      Serial.println("Moving: Clockwise " + String(moveOperationCounter));
+      myMotor->step(25, FORWARD, NumberOfStepperCoilsActivated);
+    }
+    moveOperationCounter++; // Increment the counter
     delay(PostDispenseTimeout);
   }
   else {
