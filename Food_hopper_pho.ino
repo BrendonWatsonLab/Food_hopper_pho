c7abba7a3ee4b66579422c7c35ee19969db9bde2
Pho Hale <halechr@umich.edu>
2019-03-05 11:52:24 -0500

    Initial dual-feeders implementation

--- Food_hopper_pho.ino
+++ Food_hopper_pho.ino
@@ -15,15 +15,26 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
  */
 #define IS_DIAGNOSTIC_MODE true //IS_DIAGNOSTIC_MODE: if this value is true the system will operate continuously, ignoring the beam break sensor. This serves to allow testing. This value should be false outside of testing.
 char diagnostic_val; // Data received from the serial port
-
-
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
 #define LEDPIN 13 // Pin 13: Arduino has an LED connected on pin 1
-#define SENSORPIN 4 // SENSORPIN: This pin is connected by a green wire to the beam-break sensor's "SIG" pin.
 
-// variables will change:
-int sensorState = 0, lastState=0;         // variable for reading the pushbutton status
-int moveOperationCounter = 0; // This variable keeps track of the total number of "move" operations performed.
+/* Feeder 1 config:
+ *  A "Feeder" consists of a food hopper containing pellets, a stepper motor to rotate the dispenser, a shoot, and a port. The port contains a beam-break sensor (SENSOR) that when broken dispenses food.
+ *  CONCERN: they're going to figure out that you don't have to wait as long if you alternate between the two types of food. 
+ */
+Adafruit_StepperMotor *motor1 = AFMS.getStepper(200, 2); // The motor connected to M3 & M4
+#define SENSOR1PIN 4 // SENSOR1PIN: This pin is connected by a green wire to the beam-break sensor's "SIG" pin.
+int sensor1State = 0;         // variable for reading the beam-break sensor1 status
+int moveOperationCounter1 = 0; // This variable keeps track of the total number of "move" operations performed.
+
+/* Feeder 2 config:
+ *  
+ */
+#define IS_DUAL_MOTOR_MODE true // IS_DUAL_MOTOR_MODE: if true, we use both motors. Otherwise we only use the motor connected to M3 & M4
+Adafruit_StepperMotor *motor2 = AFMS.getStepper(200, 1); // The motor connected to M1 & M2
+#define SENSOR2PIN 6 // SENSOR2PIN: This pin is connected by a green wire to the second beam-break sensor's "SIG" pin.
+int sensor2State = 0;         // variable for reading the beam-break sensor2 status
+int moveOperationCounter2 = 0; // This variable keeps track of the total number of "move" operations performed.
+
 /*
  * There are two types of "move" operations: move-clockwise, move-counterclockwise
  * The "move" operation performed depends on the moveOperationCounter. Every "ConsecutiveSameDirectionMovements + 1" steps we move counter-clockwise. Otherwise, we move clockwise.
@@ -44,8 +55,9 @@ void setup() {
   // initialize the LED pin as an output:
   pinMode(LEDPIN, OUTPUT);
   // initialize the sensor pin as an input:
-  pinMode(SENSORPIN, INPUT);     
-  digitalWrite(SENSORPIN, HIGH); // turn on the pullup
+  pinMode(SENSOR1PIN, INPUT);     
+  digitalWrite(SENSOR1PIN, HIGH); // turn on the pullup
+  
   Serial.begin(9600); // set up Serial library at 9600 bps (for debugging)
   Serial.println("Food Hopper:");
   if (IS_DIAGNOSTIC_MODE) { //If the system is in diagnostic mode, output a line to the serial terminal indicating this to prevent diagnostic builds being deployed to production hardware.
@@ -54,56 +66,118 @@ void setup() {
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   // The setSpeed() function controls the power level delivered to the motor. 
-  myMotor->setSpeed(StepperSpeed); //The speed parameter is a value between 0 and 255.
+  motor1->setSpeed(StepperSpeed); //The speed parameter is a value between 0 and 255.
+  // Set up the second feeder if needed
+  if (IS_DUAL_MOTOR_MODE) {
+     // initialize the sensor pin as an input:
+     pinMode(SENSOR2PIN, INPUT);     
+     digitalWrite(SENSOR2PIN, HIGH); // turn on the pullup
+     motor2->setSpeed(StepperSpeed); //The speed parameter is a value between 0 and 255.
+  }
 }
 
 void loop(){
   // read the state of the IR break beam sensor:
-  sensorState = digitalRead(SENSORPIN);
+  sensor1State = digitalRead(SENSOR1PIN);
+  if (IS_DUAL_MOTOR_MODE) {
+    sensor2State = digitalRead(SENSOR2PIN);
+  }
   /* Check sensor beam state:
    * LOW: Sensor Beam is broken
    * HIGH: Sensor Beam has continuity
    */
-  if ((sensorState == LOW) || IS_DIAGNOSTIC_MODE) {
+  // Currently DIAGNOSTIC_MODE Only dispenses feeder1
+  if ((sensor1State == LOW) || IS_DIAGNOSTIC_MODE) {
     //delay(40);
-    digitalWrite(LEDPIN, HIGH); // Turn status LED on
-    // The "%" operation is "modulus". The statement checks whether the moveOperationCounter is evenly divisible by ConsecutiveSameDirectionMovements. If it is, it performs the first case, otherwise the second.
-    if (((moveOperationCounter % ConsecutiveSameDirectionMovements) == 0)) {
-      //Every "ConsecutiveSameDirectionMovements + 1" steps we attempt to unjam
-      //unjamDispenseByTickTock();
-      unjamDispenseBySimpleReverse();
-    }
-    else {
-      // Otherwise we perform a normal dispense operation
-      clockwiseDispense();
-    }
-    moveOperationCounter++; // Increment the counter
-    delay(PostDispenseTimeout);
+    dispenseFeeder1();
+  }
+  else if (IS_DUAL_MOTOR_MODE && ((sensor2State == LOW) || IS_DIAGNOSTIC_MODE)) {
+    dispenseFeeder2();
   }
   else {
     // turn status LED off:
     digitalWrite(LEDPIN, LOW);  
   }
+
+  
+} // end loop
+
+
+
+void dispenseFeeder1() {
+  dispense(1);
 }
 
+void dispenseFeeder2() {
+  dispense(2);
+}
+
+// dispense(int feederNumber): takes a feeder number (currently either 1 or 2) as an argument.
+/* Called to dispense a single pellet for a single feeder, either feeder 1 or feeder 2.
+ * Keeps track of each feeders' moveOperationCounter to perform unjam operations.
+ */
+void dispense(int feederNumber) {
+    digitalWrite(LEDPIN, HIGH); // Turn status LED on
+    int moveOperationCounter = 0;
+    Adafruit_StepperMotor *activeMotor;
+    // Gets the correct moveOperationCounter value and motor pointer for the specified feederNumber
+    if (feederNumber == 1) {
+      moveOperationCounter = moveOperationCounter1;
+      activeMotor = motor1;
+    }
+    else if (feederNumber == 2) {
+      moveOperationCounter = moveOperationCounter2;
+      activeMotor = motor2;
+    }
+    else {
+      // Should never happen. Would be nice to assert.
+    }
+    
+    // The "%" operation is "modulus". The statement checks whether the moveOperationCounter is evenly divisible by ConsecutiveSameDirectionMovements. If it is, it performs the first case, otherwise the second.
+    if (((moveOperationCounter % ConsecutiveSameDirectionMovements) == 0)) {
+      //Every "ConsecutiveSameDirectionMovements + 1" steps we attempt to unjam
+      //unjamDispenseByTickTock();
+      unjamDispenseBySimpleReverse(activeMotor);
+    }
+    else {
+      // Otherwise we perform a normal dispense operation
+      clockwiseDispense(activeMotor);
+    }
+
+    // Increment the counter
+    if (feederNumber == 1) {
+      moveOperationCounter1++;
+    }
+    else if (feederNumber == 2) {
+      moveOperationCounter2++;
+    }
+    else {
+      // Should never happen. Would be nice to assert.
+    }
+    delay(PostDispenseTimeout);
+}
+
+
+
+
 // The traditional (clockwise) movement that dispenses a pellet
-void clockwiseDispense() {
+void clockwiseDispense(Adafruit_StepperMotor *activeMotor) {
   // Otherwise we move in the traditional (clockwise) direction
-  Serial.println("Moving: Clockwise " + String(moveOperationCounter));
-  myMotor->step(25, FORWARD, NumberOfStepperCoilsActivated);
+  Serial.println("Moving: Clockwise " + String(moveOperationCounter1));
+  activeMotor->step(25, FORWARD, NumberOfStepperCoilsActivated);
 }
 
 // Attempts to unjam by just moving backwards for this iteration.
-void unjamDispenseBySimpleReverse() {
-  Serial.println("Moving: Counter-Clockwise " + String(moveOperationCounter));
-  myMotor->step(25, BACKWARD, NumberOfStepperCoilsActivated); // Changed from SINGLE to DOUBLE for extra torque
+void unjamDispenseBySimpleReverse(Adafruit_StepperMotor *activeMotor) {
+  Serial.println("Moving: Counter-Clockwise " + String(moveOperationCounter1));
+  activeMotor->step(25, BACKWARD, NumberOfStepperCoilsActivated); // Changed from SINGLE to DOUBLE for extra torque
 }
 
 // Attempts to unjam by quickly moving backwards, and then forward again for this iteration. This runs the risk of double-dispensing
-void unjamDispenseByTickTock() {
-  Serial.println("Moving: UNJAM " + String(moveOperationCounter));
-  myMotor->step(25, BACKWARD, NumberOfStepperCoilsActivated); // Changed from SINGLE to DOUBLE for extra torque
-  myMotor->step(25, FORWARD, NumberOfStepperCoilsActivated); // Changed from SINGLE to DOUBLE for extra torque
+void unjamDispenseByTickTock(Adafruit_StepperMotor *activeMotor) {
+  Serial.println("Moving: UNJAM " + String(moveOperationCounter1));
+  activeMotor->step(25, BACKWARD, NumberOfStepperCoilsActivated); // Changed from SINGLE to DOUBLE for extra torque
+  activeMotor->step(25, FORWARD, NumberOfStepperCoilsActivated); // Changed from SINGLE to DOUBLE for extra torque
 }
 
 
