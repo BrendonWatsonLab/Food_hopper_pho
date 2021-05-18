d8ebb67921f5d1f568c2e834243d56b566a6f43d
CommanderPho <PhoHale@gmail.com>
2021-05-18 05:05:21 -0400

    Might be working, having Josh test

--- Common.h
+++ Common.h
@@ -57,17 +57,17 @@
 /*
  * 
  */
-#define IS_DIAGNOSTIC_MODE true //IS_DIAGNOSTIC_MODE: if this value is false, all diagnostic settings will be ignored. This value should be false outside of testing.
-//#define IS_DIAGNOSTIC_MODE false //IS_DIAGNOSTIC_MODE: if this value is false, all diagnostic settings will be ignored. This value should be false outside of testing.
-#define ENABLE_DIAGNOSTIC_SERIAL true 
-#define SHOULD_USE_INTERACTIVE_DIAGNOSTIC true //SHOULD_USE_INTERACTIVE_DIAGNOSTIC: enables extended diagnostics and testing via the helper Processing software.
+//#define IS_DIAGNOSTIC_MODE true //IS_DIAGNOSTIC_MODE: if this value is false, all diagnostic settings will be ignored. This value should be false outside of testing.
+#define IS_DIAGNOSTIC_MODE false //IS_DIAGNOSTIC_MODE: if this value is false, all diagnostic settings will be ignored. This value should be false outside of testing.
+#define ENABLE_DIAGNOSTIC_SERIAL false 
+#define SHOULD_USE_INTERACTIVE_DIAGNOSTIC false //SHOULD_USE_INTERACTIVE_DIAGNOSTIC: enables extended diagnostics and testing via the helper Processing software.
 #define INTERACTIVE_DIAGNOSTIC_SERIAL_READ_TIMEOUT 10 //INTERACTIVE_DIAGNOSTIC_SERIAL_READ_TIMEOUT: the minimum time between serial reads for interactive diagnostics
 
 // MULTIPLEXER:
 /*
  * 
  */
-const int SIGNAL_ON_TIME = 10; //msec
+const int SIGNAL_ON_TIME = 100; //msec
 
 
 // VARIABLES
