8f9a0ab5de4f21dc437d3a3d8e4062acf008e1f7
David <davskim@umich.edu>
2025-05-08 13:19:04 -0400

    fourth attempt in pushing
    man, fuck gitkraken...

--- Water_Dispenser.h
+++ Water_Dispenser.h
@@ -7,8 +7,8 @@ Servo Servo2;
 
 // reflects the open/closed state of the solenoid
 enum SolenoidState {
-  OPEN,
-  CLOSED
+    OPEN,
+    CLOSED
 };
 
 /* Water 1 config:
@@ -63,6 +63,9 @@ void setupWaterDispensers();
 void loopWaterDispensers(unsigned long currentLoopMillis);
 void closeSolenoid(int waterPortNumber);
 void openSolenoid(int waterPortNumber);
+bool turnOffSig(void* argument);
+void ServoForceOpen(void* serve);
+void ServoForceClose(void* serve);
 bool fillUp1();
 bool fillUp2();
 bool expel1();
@@ -75,143 +78,161 @@ bool expelling2 = false;
 
 // Called from setup()
 void setupWaterDispensers() {
-  // initialize the sensor pins as an input:
-  pinMode(SENSOR3PIN, INPUT);
-  digitalWrite(SENSOR3PIN, HIGH); // turn on the pullup
-  pinMode(SENSOR4PIN, INPUT);
-  digitalWrite(SENSOR4PIN, HIGH); // turn on the pullup
-  pinMode(SENSOR5PIN, INPUT);
-  digitalWrite(SENSOR5PIN, HIGH);
-  pinMode(SENSOR6PIN, INPUT);
-  digitalWrite(SENSOR6PIN, HIGH);
-  pinMode(EXPELPIN1, INPUT);
-  digitalWrite(EXPELPIN1,HIGH);
-  pinMode(EXPELPIN2, INPUT);
-  digitalWrite(EXPELPIN2,HIGH);
+    // initialize the sensor pins as an input:
+    pinMode(SENSOR3PIN, INPUT);
+    digitalWrite(SENSOR3PIN, HIGH); // turn on the pullup
+    pinMode(SENSOR4PIN, INPUT);
+    digitalWrite(SENSOR4PIN, HIGH); // turn on the pullup
+    pinMode(SENSOR5PIN, INPUT);
+    digitalWrite(SENSOR5PIN, HIGH);
+    pinMode(SENSOR6PIN, INPUT);
+    digitalWrite(SENSOR6PIN, HIGH);
+    pinMode(EXPELPIN1, INPUT);
+    digitalWrite(EXPELPIN1, HIGH);
+    pinMode(EXPELPIN2, INPUT);
+    digitalWrite(EXPELPIN2, HIGH);
 
 
-  // Setup Servos
-  Servo1.attach(SOLENOID1PIN);
+    // Setup Servos
+    Servo1.attach(SOLENOID1PIN);
 
-  Servo2.attach(SOLENOID2PIN);
+    Servo2.attach(SOLENOID2PIN);
 }
 
 
 //allows independent solenoid operation (simultaneous)
 void loopWaterDispensers(unsigned long currentLoopMillis) {
-  // For any open solenoid, check to see if it's time to close it.
-    // If it's not, do nothing.
-  // For any closed solenoid, check to see if the beam-break sensor is blocked
-    // If it is, open it.
-    
-  // Check Water Port 1:
-  if (solenoid1State == OPEN) {
-    if (currentLoopMillis - lastSolenoidOpenTimer1 >= SolenoidDoseOpenDuration) {
-      // Close the solenoid
-      closeSolenoid(1);
-    }
-  }
-  else { // else the solenoid is CLOSED
-    // Check if at least SolenoidPostDoseClosedDuration msec have passed since the last solenoid close event (to prevent immediate re-opening).
-    if (currentLoopMillis - lastSolenoidCloseTimer1 >= SolenoidPostDoseClosedDuration) {
-      /* Check sensor beam state:
-          LOW: Sensor Beam is broken
-          HIGH: Sensor Beam has continuity
-      */
-      // The sensor must have changed state after the end of the last water dispense and timeout period
-      #if REQUIRE_STATE_CHANGE_BEFORE_SECOND_WATER_DISPENSE
-      if (lastSensorChangeEvent3  > (lastSolenoidCloseTimer1 + SolenoidPostDoseClosedDuration)) {
-      #endif
-        if ((sensor3State == LOW) || (IS_DIAGNOSTIC_MODE && DIAGNOSTIC_SHOULD_CONTINUOUSLY_DISPENSE_WATER)) {
-          #if ENABLE_LOGGING_SIGNAL_ON_CHANGE
-            sendLoggingSignal(Water1, ActionDispense);
-          #endif
-          openSolenoid(1);
+    // For any open solenoid, check to see if it's time to close it.
+      // If it's not, do nothing.
+    // For any closed solenoid, check to see if the beam-break sensor is blocked
+      // If it is, open it.
+
+    // Check Water Port 1:
+    if (solenoid1State == OPEN) {
+        if (currentLoopMillis - lastSolenoidOpenTimer1 >= SolenoidDoseOpenDuration) {
+            // Close the solenoid
+            closeSolenoid(1);
         }
-      #if REQUIRE_STATE_CHANGE_BEFORE_SECOND_WATER_DISPENSE
-      }
-      #endif
     }
-  }
-
-  // Check Water Port 2:
-  if (solenoid2State == OPEN) {
-    if (currentLoopMillis - lastSolenoidOpenTimer2 >= SolenoidDoseOpenDuration) {
-      // Close the solenoid
-      closeSolenoid(2);
-    }
-  }
-  else { // else the solenoid is CLOSED
-     // Check if at least SolenoidPostDoseClosedDuration msec have passed since the last solenoid close event (to prevent immediate re-opening).
-    if (currentLoopMillis - lastSolenoidCloseTimer2 >= SolenoidPostDoseClosedDuration) {
-      /* Check sensor beam state:
-          LOW: Sensor Beam is broken
-          HIGH: Sensor Beam has continuity
-        */
-       // The sensor must have changed state after the end of the last water dispense and timeout period
-      #if REQUIRE_STATE_CHANGE_BEFORE_SECOND_WATER_DISPENSE
-      if (lastSensorChangeEvent4  > (lastSolenoidCloseTimer2 + SolenoidPostDoseClosedDuration)) {
-      #endif
-        if ((sensor4State == LOW)) {
-            #if ENABLE_LOGGING_SIGNAL_ON_CHANGE
-              sendLoggingSignal(Water2, ActionDispense);
-            #endif
-            openSolenoid(2);
+    else { // else the solenoid is CLOSED
+        // Check if at least SolenoidPostDoseClosedDuration msec have passed since the last solenoid close event (to prevent immediate re-opening).
+        if (currentLoopMillis - lastSolenoidCloseTimer1 >= SolenoidPostDoseClosedDuration) {
+            /* Check sensor beam state:
+                LOW: Sensor Beam is broken
+                HIGH: Sensor Beam has continuity
+            */
+            // The sensor must have changed state after the end of the last water dispense and timeout period
+#if REQUIRE_STATE_CHANGE_BEFORE_SECOND_WATER_DISPENSE
+            if (lastSensorChangeEvent3 > (lastSolenoidCloseTimer1 + SolenoidPostDoseClosedDuration)) {
+#endif
+                if ((sensor3State == LOW) || (IS_DIAGNOSTIC_MODE && DIAGNOSTIC_SHOULD_CONTINUOUSLY_DISPENSE_WATER)) {
+#if ENABLE_LOGGING_SIGNAL_ON_CHANGE
+                    sendLoggingSignal(Water1, ActionDispense);
+#endif
+                    openSolenoid(1);
+                }
+#if REQUIRE_STATE_CHANGE_BEFORE_SECOND_WATER_DISPENSE
+            }
+#endif
         }
-      #if REQUIRE_STATE_CHANGE_BEFORE_SECOND_WATER_DISPENSE
-      }
-      #endif
     }
-  }
 
-  
-  if (sensor5State == LOW){
-    hasbeenfilled1 = fillUp1();
-  } else if (hasbeenfilled1) {
-      closeSolenoid(1);
-      hasbeenfilled1 = false;
-      Serial.write("wtf");
-  }
-  if (sensor6State == LOW){
-    hasbeenfilled2 = fillUp2();
-  } else if (hasbeenfilled2){
-      closeSolenoid(2);
-      hasbeenfilled2 = false;
-      Serial.write("wtf");
-  }
-  
-  // This is the code for expelling the servo pumps
-  if (expel1state == LOW){
-    expelling1 = expel1();
-  } else if (expelling1) {
-      closeSolenoid(1);
-      expelling1 = false;
-  }
-  if (expel2state == LOW){
-    expelling2 = expel2();
-  } else if (expelling2){
-      closeSolenoid(2);
-      expelling2 = false;
-  }
+    // Check Water Port 2:
+    if (solenoid2State == OPEN) {
+        if (currentLoopMillis - lastSolenoidOpenTimer2 >= SolenoidDoseOpenDuration) {
+            // Close the solenoid
+            closeSolenoid(2);
+        }
+    }
+    else { // else the solenoid is CLOSED
+        // Check if at least SolenoidPostDoseClosedDuration msec have passed since the last solenoid close event (to prevent immediate re-opening).
+        if (currentLoopMillis - lastSolenoidCloseTimer2 >= SolenoidPostDoseClosedDuration) {
+            /* Check sensor beam state:
+                LOW: Sensor Beam is broken
+                HIGH: Sensor Beam has continuity
+              */
+              // The sensor must have changed state after the end of the last water dispense and timeout period
+#if REQUIRE_STATE_CHANGE_BEFORE_SECOND_WATER_DISPENSE
+            if (lastSensorChangeEvent4 > (lastSolenoidCloseTimer2 + SolenoidPostDoseClosedDuration)) {
+#endif
+                if ((sensor4State == LOW)) {
+#if ENABLE_LOGGING_SIGNAL_ON_CHANGE
+                    sendLoggingSignal(Water2, ActionDispense);
+#endif
+                    openSolenoid(2);
+                }
+#if REQUIRE_STATE_CHANGE_BEFORE_SECOND_WATER_DISPENSE
+            }
+#endif
+        }
+    }
 
 
-// Below is functionality that would dispense water once every TOP_UP_TIME
-  if (currentLoopMillis > topup + TOP_UP_TIME) {
-    openSolenoid(1);
-    openSolenoid(2);
-    Serial.write("TOP UP");
-    topupGate = true;
-    //Sending Output signals to LJ
-    digitalWrite(23, LOW);
-    timer.in(SIGNAL_ON_TIME, turnOffSignal, 23); 
-    digitalWrite(25, LOW);
-    timer.in(SIGNAL_ON_TIME, turnOffSignal, 25); 
-  } else if (topupGate && (currentLoopMillis > 1500 + TOP_UP_TIME + topup)) {
-    topup = currentLoopMillis;
-    topupGate = false;
-    closeSolenoid(1);
-    closeSolenoid(2);
-  }
+    if (sensor5State == LOW) {
+        hasbeenfilled1 = fillUp1();
+    }
+    else if (hasbeenfilled1) {
+        closeSolenoid(1);
+        hasbeenfilled1 = false;
+        Serial.write("wtf");
+    }
+    if (sensor6State == LOW) {
+        hasbeenfilled2 = fillUp2();
+    }
+    else if (hasbeenfilled2) {
+        closeSolenoid(2);
+        hasbeenfilled2 = false;
+        Serial.write("wtf");
+    }
+
+    // This is the code for expelling the servo pumps
+    if (expel1state == LOW) {
+        expelling1 = expel1();
+    }
+    else if (expelling1) {
+        closeSolenoid(1);
+        expelling1 = false;
+    }
+    if (expel2state == LOW) {
+        expelling2 = expel2();
+    }
+    else if (expelling2) {
+        closeSolenoid(2);
+        expelling2 = false;
+    }
+
+
+    // Below is functionality that would dispense water once every TOP_UP_TIME
+    //  if (!topupGate && (currentLoopMillis < topup + 1500)) {
+    //    openSolenoid(1);
+    //    openSolenoid(2);
+    //    Serial.write("TOP UP");
+    //    topupGate = true;
+    //    //Sending Output signals to LJ
+    ////    digitalWrite(23, LOW);
+    ////    digitalWrite(25, LOW); 
+    //  } else if (topupGate && (currentLoopMillis > TOP_UP_TIME + topup)) {
+    //    topup = currentLoopMillis;
+    //    topupGate = false;
+    //    closeSolenoid(1);
+    //    closeSolenoid(2);
+    ////    digitalWrite(23, HIGH);
+    ////    digitalWrite(25, HIGH); 
+    //  }
+
+    if (currentLoopMillis > topup + TOP_UP_TIME) {
+        topup = currentLoopMillis;
+        Servo1.write(105);
+        Servo2.write(105);
+        digitalWrite(23, LOW);
+        digitalWrite(25, LOW);
+        Serial.write("Topped up");
+
+        timer.in(1500, ServoForceClose, 1);
+        timer.in(1500, ServoForceClose, 2);
+        timer.in(SIGNAL_ON_TIME, turnOffSig, 23);
+        timer.in(SIGNAL_ON_TIME, turnOffSig, 25);
+    }
 
 }
 
@@ -221,39 +242,39 @@ void loopWaterDispensers(unsigned long currentLoopMillis) {
 
 
 void closeSolenoid(int waterPortNumber) {
-  int activeSolenoidPin = 0;
-  Servo *activeServo;
-  if (waterPortNumber == 1) {
-    activeServo = &Servo1;
-  }
-  else if (waterPortNumber == 2) {
-    activeServo = &Servo2;
-  }
-  else {
-    // Should never happen. Would be nice to assert.
-    Serial.println("----- waterPortNumber Error A! -----");
-  }
-  // Actually close the solenoid and save the time it was closed
-  activeServo->write(90);
-  
-  if (waterPortNumber == 1) {
-    lastSolenoidCloseTimer1 = millis();
-    if (solenoid1State == OPEN) {
-      solenoid1State = CLOSED; // update the state to closed
-      moveOperationCounter3++; // when the solenoid is transitioned from opened to closed, count that as a move operation
+    int activeSolenoidPin = 0;
+    Servo* activeServo;
+    if (waterPortNumber == 1) {
+        activeServo = &Servo1;
     }
-  }
-  else if (waterPortNumber == 2) {
-    lastSolenoidCloseTimer2 = millis();
-    if (solenoid2State == OPEN) {
-      solenoid2State = CLOSED; // update the state to closed
-      moveOperationCounter4++; // when the solenoid is transitioned from opened to closed, count that as a move operation
+    else if (waterPortNumber == 2) {
+        activeServo = &Servo2;
     }
-  }
-  else {
-    // Should never happen. Would be nice to assert.
-    Serial.println("----- waterPortNumber Error B! -----");
-  }
+    else {
+        // Should never happen. Would be nice to assert.
+        Serial.println("----- waterPortNumber Error A! -----");
+    }
+    // Actually close the solenoid and save the time it was closed
+    activeServo->write(90);
+
+    if (waterPortNumber == 1) {
+        lastSolenoidCloseTimer1 = millis();
+        if (solenoid1State == OPEN) {
+            solenoid1State = CLOSED; // update the state to closed
+            moveOperationCounter3++; // when the solenoid is transitioned from opened to closed, count that as a move operation
+        }
+    }
+    else if (waterPortNumber == 2) {
+        lastSolenoidCloseTimer2 = millis();
+        if (solenoid2State == OPEN) {
+            solenoid2State = CLOSED; // update the state to closed
+            moveOperationCounter4++; // when the solenoid is transitioned from opened to closed, count that as a move operation
+        }
+    }
+    else {
+        // Should never happen. Would be nice to assert.
+        Serial.println("----- waterPortNumber Error B! -----");
+    }
 }
 
 
@@ -261,32 +282,32 @@ void closeSolenoid(int waterPortNumber) {
 
 
 void openSolenoid(int waterPortNumber) {
-  int activeSolenoidPin = 0;
-  Servo *activeServo;
-  if (waterPortNumber == 1) {
-    activeServo = &Servo1;
-    solenoid1State = OPEN; // update the state to closed before actually closing it (just to avoid another if/elseif/else block)
-  }
-  else if (waterPortNumber == 2) {
-    activeServo = &Servo2;
-    solenoid2State = OPEN; // update the state to closed before actually closing it (just to avoid another if/elseif/else block)
-  }
-  else {
-    // Should never happen. Would be nice to assert.
-    Serial.println("----- waterPortNumber Error A! -----");
-  }
-  // Actually close the solenoid and save the time it was closed
-  activeServo->write(105);
-  if (waterPortNumber == 1) {
-    lastSolenoidOpenTimer1 = millis();
-  }
-  else if (waterPortNumber == 2) {
-    lastSolenoidOpenTimer2 = millis();
-  }
-  else {
-    // Should never happen. Would be nice to assert.
-    Serial.println("----- waterPortNumber Error B! -----");
-  }
+    int activeSolenoidPin = 0;
+    Servo* activeServo;
+    if (waterPortNumber == 1) {
+        activeServo = &Servo1;
+        solenoid1State = OPEN; // update the state to closed before actually closing it (just to avoid another if/elseif/else block)
+    }
+    else if (waterPortNumber == 2) {
+        activeServo = &Servo2;
+        solenoid2State = OPEN; // update the state to closed before actually closing it (just to avoid another if/elseif/else block)
+    }
+    else {
+        // Should never happen. Would be nice to assert.
+        Serial.println("----- waterPortNumber Error A! -----");
+    }
+    // Actually open the solenoid and save the time it was closed
+    activeServo->write(105);
+    if (waterPortNumber == 1) {
+        lastSolenoidOpenTimer1 = millis();
+    }
+    else if (waterPortNumber == 2) {
+        lastSolenoidOpenTimer2 = millis();
+    }
+    else {
+        // Should never happen. Would be nice to assert.
+        Serial.println("----- waterPortNumber Error B! -----");
+    }
 }
 
 
@@ -294,36 +315,61 @@ void openSolenoid(int waterPortNumber) {
 
 
 bool fillUp1() {
-  int activeSolenoidPin = 0;
-  Servo *activeServo = &Servo1;
-  // pull back on syringe to refill water
-  activeServo->write(0);
-  Serial.write("fillUp1");
-  return true;
+    Servo* activeServo = &Servo1;
+    // pull back on syringe to refill water
+    activeServo->write(0);
+    Serial.write("fillUp1");
+    return true;
 }
 
 bool fillUp2() {
-  int activeSolenoidPin = 0;
-  Servo *activeServo = &Servo2;
-  activeServo->write(0);
-  Serial.write("fillUp2");
-  return true;
-}  
+    Servo* activeServo = &Servo2;
+    activeServo->write(0);
+    Serial.write("fillUp2");
+    return true;
+}
 
 bool expel1() {
-  int activeSolenoidPin = 0;
-  Servo *activeServo = &Servo1;
-  // pull back on syringe to refill water
-  activeServo->write(180);
-  Serial.write("Expel1");
-  return true;
+    Servo* activeServo = &Servo1;
+    // pull back on syringe to refill water
+    activeServo->write(180);
+    Serial.write("Expel1");
+    return true;
 }
 
 bool expel2() {
-  int activeSolenoidPin = 0;
-  Servo *activeServo = &Servo2;
-  activeServo->write(180);
-  Serial.write("Expel2");
-  return true;
-}  
-   
+    Servo* activeServo = &Servo2;
+    activeServo->write(180);
+    Serial.write("Expel2");
+    return true;
+}
+
+// This is copied from the Mega2560Labjack_interface.h file. 
+/* This is needed because timer.in() function used for timing dispenses can
+only accept function handles that contain a single argument... This sucks lowkey...
+One of the few times that I actually wish we were in python because of the tuples...*/
+bool turnOffSig(void* argument) {
+    digitalWrite(argument, HIGH);
+    return false; // to repeat the action - false to stop
+}
+
+void ServoForceOpen(void* serve) {
+    if (serve == 1) {
+        Servo1.write(105);
+    }
+    else if (serve == 2) {
+        Servo2.write(105);
+    }
+}
+
+/* I needed this for the same reason as the turnOffSign above...
+ * I have ServoForceOpen as well just in case if we need this in the future
+ */
+void ServoForceClose(void* serve) {
+    if (serve == 1) {
+        Servo1.write(90);
+    }
+    else if (serve == 2) {
+        Servo2.write(90);
+    }
+}
