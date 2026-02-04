e5b60b1c9925ae1a73fc1acdffa414f0a5d44d77
avery <averygj@umich.edu>
2026-02-04 12:18:01 -0500

    Changing pins for filling water syringe
    Using pins 19 and 20

--- Water_Dispenser.h
+++ Water_Dispenser.h
@@ -42,8 +42,8 @@ int sensor5State = HIGH;
 int sensor6State = HIGH;
 int expel1state = HIGH;
 int expel2state = HIGH;
-#define SENSOR5PIN 12
-#define SENSOR6PIN 13
+#define SENSOR5PIN 19
+#define SENSOR6PIN 20
 #define EXPELPIN1 18
 #define EXPELPIN2 19
 
